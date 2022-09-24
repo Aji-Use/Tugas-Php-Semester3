bc5de4adf3ce07eff56864a946f6fe073ec129bf
Aji-Use <114288438+Aji-Use@users.noreply.github.com>
2022-09-24 19:21:51 +0700

    Oop modul2

--- oop_modul2.php
+++ oop_modul2.php
@@ -19,32 +19,14 @@
       }  
  }  
    
-//  class Kambing Implements Hewan {  
-//       public function Makan() {  
-//            return "Kambing makan rumput<br/>";  
-//       }  
-             
-//       public function Bergerak() {  
-//            return "Kambing bergerak dengan berjalan dan berlari<br/>";  
-//       }  
-        
-//       public function Beranak() {  
-//            return "Kambing beranak dengan melahirkan<br/>";  
-//       }  
-//  }  
    
  $monyet = new Monyet;  
-//  $kambing = new Kambing;  
+  
    
 
      echo "<b>Kemampuan Monyet : </b><br/>";  
      echo $monyet->Makan();  
      echo $monyet->Berjalan();  
      echo $monyet->Bersuara();  
-
-//  echo "<br/>";  
-//  echo "<b>Kemampuan lainnya pada Monyet : </b><br/>";  
-//  echo $kambing->Makan();  
-//  echo $kambing->Bergerak();  
-//  echo $kambing->Beranak();  
+  
  ?>    
